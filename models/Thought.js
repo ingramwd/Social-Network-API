846270377b5eb6822e21177c14706cb8210906af
ingramwd <Ingramwd01@gmail.com>
2022-03-13 16:39:42 -0400

    finished reactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,26 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
 
 
 const ThoughtSchema = new Schema(
