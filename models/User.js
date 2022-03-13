166646ee83cfa49d52da1f18ea4a463b861e1bfd
ingramwd <Ingramwd01@gmail.com>
2022-03-13 16:20:44 -0400

    finished User model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+\@.+\..+/, 'Please enter a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+//to get the length of the fiends array field on query
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+model.exports = User;
